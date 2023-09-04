c482e3d153b2ceb4d9f2006ba7f70b8160f0e22b
y-marchuk <yuliia.marchuk@kinesso.com>
2023-09-04 17:26:02 +0300

    add .idea to gitignore

--- src/main/java/com/template/project/AppProperty.java
+++ src/main/java/com/template/project/AppProperty.java
@@ -1,6 +1,6 @@
 package com.template.project;
 
 public class AppProperty {
-    static final String BOT_TOKEN = "6498406682:AAGc4d7Bu72FEoIVVTPrK2316vb_KOT78eE";
-    static final String BOT_USERNAME = "Learning_1177_bot";
+    static final String BOT_TOKEN = "";
+    static final String BOT_USERNAME = "";
 }
